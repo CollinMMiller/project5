2edde360220dd3f7a26048a355ca7bb9d628a487
zoez782892546 <zoez@vt.edu>
2019-11-17 16:53:28 -0500

    reader and student

---
+++ src/prj5/StudentTest.java
@@ -0,0 +1,30 @@
+package prj5;
+
+
+import student.TestCase;
+/**
+ * StudentTest class
+ *
+ * @author <Zoe Zheng> <zoez>
+ * @version <11/17/2019>
+ */
+public class StudentTest extends TestCase{
+
+    private Student s;
+    private LinkedList<Song> h;
+    private LinkedList<Song> l;
+    private MajorEnum m;
+    private RegionEnum r;
+    private HobbyEnum hobby;
+    //private Song song;
+    
+    /**
+     * Creates two brand new, empty sets for each test method.
+     */
+    public void setUp() {
+        h.add(new Song("t","a",2015,"G"));
+        l.add(new Song("t","a",2015,"G"));
+        s = new Student(m,r,hobby,h,l);
+
+    }
+}
